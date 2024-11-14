2117a862411e9c4891f57a7052ddcb517ceb0ad5
jc2502 <jc2003jj@gmail.com>
2024-11-14 12:41:30 -0400

    Creación de Acerca de

---
+++ AppNetRazor/Pages/Acerca.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AppNetRazor.Pages
+{
+    public class AcercaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
